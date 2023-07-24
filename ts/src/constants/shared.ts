887c3e3367e33e5ec8bef7db1523d2f06be5df23
hoak <hoak@offpiste.io>
2023-07-24 06:33:55 +0100

    updating rpc urls

--- ts/src/constants/shared.ts
+++ ts/src/constants/shared.ts
@@ -16,14 +16,13 @@ export const CONFIGS: { [key in Cluster]: Config } = {
     HISTORY_API_GRAPHQL: 'http://localhost:8081/v1/graphql'
   },
   devnet: {
-    RPC_ENDPOINT:
-      'https://cypher-develope-1013.devnet.rpcpool.com/1a4c1f68-bf8e-4b64-9d41-8e5b4032ef21',
+    RPC_ENDPOINT: 'https://bankmen-main71f-f62a.devnet.rpcpool.com',
     PROGRAM_ID: new PublicKey('2Kdt8uMA6m5stQqaTxVPac45j6uKbwCg5vaPtyqwLk5C'),
     HISTORY_API_GRAPHQL: 'https://gbg-lending-prod.hasura.app/v1/graphql'
   },
   'mainnet-beta': {
     RPC_ENDPOINT:
-      'https://cypher-main-0965.mainnet.rpcpool.com/df807581-f194-4ddd-9959-855c396814ad',
+      'https://bankmen-main71f-f62a.mainnet.rpcpool.com/f6f0b0ad-6b0f-4a33-95eb-8944a9474e6f',
     PROGRAM_ID: new PublicKey('BMfi6hbCSpTS962EZjwaa6bRvy2izUCmZrpBMuhJ1BUW'),
     HISTORY_API_GRAPHQL: 'https://gbg-lending.hasura.app/v1/graphql'
   }
