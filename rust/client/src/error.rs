4c25c77a29196c21673d6f6680f59b8f17df4893
hoak <hoak@offpiste.io>
2023-09-13 16:22:06 +0100

    removing rust client and cli because typescript has caught up

--- rust/client/src/error.rs
+++
@@ -1,8 +0,0 @@
-use solana_client::client_error::ClientError;
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum LendingClientError {
-    #[error(transparent)]
-    ClientError(#[from] ClientError),
-}
