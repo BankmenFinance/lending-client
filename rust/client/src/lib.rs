a23a1ddf88ec6d9fdcb15b3b9035047385c0efd9
hoak <hoak@offpiste.io>
2023-09-20 01:55:03 +0100

    rust: adding fmt::Debug impls for events

--- rust/client/src/lib.rs
+++ rust/client/src/lib.rs
@@ -1,3 +1,5 @@
+use std::str::from_utf8;
+
 use anchor_lang::prelude::*;
 
 anchor_gen::generate_cpi_interface!(idl_path = "idl.json");
@@ -24,5 +26,138 @@ impl PartialEq for LoanType {
             (LoanType::LoanToValue, LoanType::Simple) => true,
             (LoanType::LoanToValue, LoanType::LoanToValue) => false,
         }
+    }
+}
+
+impl std::fmt::Debug for CollectionLendingProfileCreated {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CollectionLendingProfileCreated")
+            .field("profile", &self.profile)
+            .field("collection", &self.collection)
+            .field(
+                "collection_name",
+                &from_utf8(&self.collection_name)
+                    .unwrap()
+                    .trim_matches(char::from(0)),
+            )
+            .field("id", &self.id)
+            .field("loan_token_mint", &self.loan_token_mint)
+            .field("fee_rate", &self.fee_rate)
+            .field("interest_rate", &self.interest_rate)
+            .field("loan_duration", &self.loan_duration)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for CollectionLendingProfileParamsChange {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CollectionLendingProfileParamsChange")
+            .field("profile", &self.profile)
+            .field("fee_rate", &self.fee_rate)
+            .field("interest_rate", &self.interest_rate)
+            .field("loan_duration", &self.loan_duration)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for CollectionLendingProfileLtvEnabled {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CollectionLendingProfileLtvEnabled")
+            .field("profile", &self.profile)
+            .field("oracle", &self.oracle)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for CollectionLendingProfileLtvDisabled {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CollectionLendingProfileLtvDisabled")
+            .field("profile", &self.profile)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for CollectionLendingProfileClosed {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CollectionLendingProfileClosed")
+            .field("profile", &self.profile)
+            .field("collection", &self.collection)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for CollectionLendingProfileStatusChange {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CollectionLendingProfileStatusChange")
+            .field("profile", &self.profile)
+            .field("status", &self.status)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for LoanOfferCreated {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("LoanOfferCreated")
+            .field("profile", &self.profile)
+            .field("loan", &self.loan)
+            .field("loan_mint", &self.loan_mint)
+            .field("lender", &self.lender)
+            .field("lender_account", &self.lender_account)
+            .field("loan_type", &self.loan_type)
+            .field("amount", &self.amount)
+            .field("ltv_amount", &self.ltv_amount)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for LoanOfferCanceled {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("LoanOfferCanceled")
+            .field("profile", &self.profile)
+            .field("loan", &self.loan)
+            .field("lender", &self.lender)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for LoanOrigination {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("LoanOrigination")
+            .field("profile", &self.profile)
+            .field("loan", &self.loan)
+            .field("lender", &self.lender)
+            .field("borrower", &self.borrower)
+            .field("borrower_account", &self.borrower_account)
+            .field("loan_amount", &self.loan_amount)
+            .field("repayment_amount", &self.repayment_amount)
+            .field("due_timestamp", &self.due_timestamp)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for LoanRepayment {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("LoanRepayment")
+            .field("profile", &self.profile)
+            .field("loan", &self.loan)
+            .field("lender", &self.lender)
+            .field("borrower", &self.borrower)
+            .field("paid_amount", &self.paid_amount)
+            .field("amount_left", &self.amount_left)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for LoanForeclosed {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("LoanForeclosed")
+            .field("profile", &self.profile)
+            .field("loan", &self.loan)
+            .field("lender", &self.lender)
+            .field("borrower", &self.borrower)
+            .field("paid_amount", &self.paid_amount)
+            .field("amount_left", &self.amount_left)
+            .field("timestamp", &self.timestamp)
+            .finish()
     }
 }
